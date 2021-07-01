cc7fa30eafa76f3e9219f7794fccdfe1a882a692
Steve Degosserie <steve@parity.io>
2021-07-01 11:29:40 +0200

    Update WalChain node to latest Substrate template (#6)
    Merge remote-tracking branch 'upstream/master' commit #e54c97edd8eaff2e9eda83c1b8575d63c084c218

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-# This script meant to be run on Unix/Linux based systems
+# This script is meant to be run on Unix/Linux based systems
 set -e
 
 echo "*** Start Substrate node template ***"
 
 cd $(dirname ${BASH_SOURCE[0]})/..
+
+mkdir -p ./.local
 
 docker-compose down --remove-orphans
 docker-compose run --rm --service-ports dev $@
